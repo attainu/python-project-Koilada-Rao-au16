89b8ce7a61d740c99b570613a1458f0481435f77
Bhaskar <bhaskar9.koilada@gmail.com>
2021-02-18 22:07:56 +0530

    files move extension wise by movables method

--- Junk_file_Organizer/extesion_class/exten.py
+++ Junk_file_Organizer/extesion_class/exten.py
@@ -10,3 +10,17 @@ class Exten:
             file_format = file_format[1::]
             File_extension.append(file_format)
 
+    def directoryCreation(self, file_extension, dir_path):
+        for dir in file_extension:
+            directories_path = os.path.join(dir_path, dir)
+            directories_path = Path(directories_path)
+            directories_path.mkdir(exist_ok = True)
+
+    def movables(self, all_files, dir_path):
+        for files in all_files:
+            file_path = path(files)
+            extension = file_path.suffix.lower()
+            extension = extension[1::]
+            old_path  = os.path.join(dir_path, file_path)
+            new_path  = os.path.join(dir_path, extension, files)
+            os.rename(old_path, new_path)
